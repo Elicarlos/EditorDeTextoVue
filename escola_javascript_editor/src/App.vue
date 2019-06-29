fefab82c62be1679e5068c10b2544382774cfc4a
Elicarlos <elicarlossantos_@hotmail.com>
2019-06-28 23:40:48 -0300

    Editor Finalizado

--- escola_javascript_editor/src/App.vue
+++ escola_javascript_editor/src/App.vue
@@ -3,25 +3,24 @@
     <editor/>
   </div>
 </template>
-
+ 
 <script>
-import Editor from './components/Editor.vue'
-
+import Editor from "./components/Editor.vue";
+ 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Editor
   }
-}
+};
 </script>
-
+ 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
